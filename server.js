2843f76181ea1996b3a0514cfff4493fff1d4a38
sebagruia <blackthorus@gmail.com>
2021-01-27 22:06:04 +0100

    Debugging 400 error

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ app.get("/contentful/posts", async (req, res) => {
 app.post("/booking", async (req, res) => {
   console.log(req.body);
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST");
   const data = {
     service_id: process.env.EMAILJS_YOUR_SERVICE_ID,
     template_id: req.body.date
