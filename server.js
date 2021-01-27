bd4e349ef115782a0826db5c0254cd964b86a744
sebagruia <blackthorus@gmail.com>
2021-01-27 14:47:25 +0100

    Added endpoit for sending Emails

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
 
 const contentful = require("contentful");
 
@@ -11,6 +12,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
@@ -28,23 +31,51 @@ app.get("/contentful/homeContent", async (req, res) => {
 });
 
 app.get("/contentful/testimonials", async (req, res) => {
-  const testimonialsHome = await client.getEntries({content_type:'testimonial'});
+  const testimonialsHome = await client.getEntries({
+    content_type: "testimonial",
+  });
   res.send(testimonialsHome);
 });
 
 app.get("/contentful/aboutUs", async (req, res) => {
-  const aboutUs = await client.getEntry('6HiiszYhxuNF18wxss5Ijd');
+  const aboutUs = await client.getEntry("6HiiszYhxuNF18wxss5Ijd");
   res.send(aboutUs);
 });
 
 app.get("/contentful/services", async (req, res) => {
-  const services = await client.getEntries({content_type:'services'});
+  const services = await client.getEntries({ content_type: "services" });
   res.send(services);
 });
 
 app.get("/contentful/posts", async (req, res) => {
-  const posts = await client.getEntries({content_type:'post'});
+  const posts = await client.getEntries({ content_type: "post" });
   res.send(posts);
+});
+
+app.post("/booking", async (req, res) => {
+  const data = {
+    service_id: process.env.EMAILJS_YOUR_SERVICE_ID,
+    template_id: req.body.date
+      ? process.env.EMAILJS_YOUR_TEMPLATE_ID
+      : process.env.EMAILJS_YOUR_NEW_MESSAGE_TEMPLATE,
+    template_params: req.body,
+    user_id: process.env.EMAILJS_YOUR_USER_ID,
+  };
+  try {
+    const fetchResponse = await fetch(
+      "https://api.emailjs.com/api/v1.0/email/send",
+      {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+        contentType: "application/json",
+      }
+    );
+
+    res.sendStatus(fetchResponse.status);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 app.get("/", (req, res) => {
